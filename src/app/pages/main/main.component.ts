416057a33f006b0da9767f9038c12cbf8b882a43
cinta <cintatafur@gmail.com>
2023-02-02 19:16:25 +0100

    🐛 FIx(bug)

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -8,27 +8,54 @@ import { RyM } from 'src/app/interfaces/RyM';
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss'],
 })
-export class MainComponent implements OnInit{
+export class MainComponent {
   rickMorty: RyM[] | undefined;
-
+  rickMortyCopy: RyM[] | undefined;
 
   constructor(public http: HttpClient) {
-    
+    this.getRyM()
   }
-  ngOnInit(): void {
-    this.http.get<any>(environment.apiUrl).subscribe(res => {
-    console.log(res);
-    this.rickMorty = res.results;
-    })
+ async getRyM() {
+   await this.http.get<any>(environment.apiUrl).subscribe((res) => {
+      this.rickMorty = res.map(
+        ({
+          id,
+          name,
+          status,
+          species,
+          type,
+          gender,
+          origin,
+          location,
+          image,
+          url,
+          created,
+        }: RyM) => {
+          return {
+            id: id,
+            name: name,
+            status: status,
+            species: species,
+            type: type,
+            gende: gender,
+            origin: origin,
+            location: location,
+            image: image,
+            url: url,
+            created: created,
+          };
+        }
+      );
+      this.rickMortyCopy = this.rickMorty;
+    });
   }
 
-   
-  
-  
   filtro(e: any) {
     const buscardor: string = e.target.value;
-    console.log({ buscardor });
-    console.log(buscardor);
+    console.log({buscardor});
+    this.rickMorty = this.rickMortyCopy?.filter(({ name }: RyM) => {
+      return name.toLowerCase().includes(buscardor.toLowerCase());
+    });
     //TODO:hacer filter
   }
 }
