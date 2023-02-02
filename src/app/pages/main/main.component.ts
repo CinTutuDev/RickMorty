d078f15b4195007ace52e0b9b88d748621e3d9b8
cinta <cintatafur@gmail.com>
2023-02-02 19:40:04 +0100

    🍻 FIn del proyecto

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -13,49 +13,20 @@ export class MainComponent {
   rickMortyCopy: RyM[] | undefined;
 
   constructor(public http: HttpClient) {
-    this.getRyM()
+    this.getRyM();
   }
- async getRyM() {
-   await this.http.get<any>(environment.apiUrl).subscribe((res) => {
-      this.rickMorty = res.map(
-        ({
-          id,
-          name,
-          status,
-          species,
-          type,
-          gender,
-          origin,
-          location,
-          image,
-          url,
-          created,
-        }: RyM) => {
-          return {
-            id: id,
-            name: name,
-            status: status,
-            species: species,
-            type: type,
-            gende: gender,
-            origin: origin,
-            location: location,
-            image: image,
-            url: url,
-            created: created,
-          };
-        }
-      );
+  async getRyM() {
+    await this.http.get<any>(environment.apiUrl).subscribe((res) => {
+      this.rickMorty = res.results;
       this.rickMortyCopy = this.rickMorty;
     });
   }
 
   filtro(e: any) {
     const buscardor: string = e.target.value;
-    console.log({buscardor});
+    console.log({ buscardor });
     this.rickMorty = this.rickMortyCopy?.filter(({ name }: RyM) => {
       return name.toLowerCase().includes(buscardor.toLowerCase());
     });
-    //TODO:hacer filter
   }
 }
