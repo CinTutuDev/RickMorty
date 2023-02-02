4b62c244c46b537d6ce64122df024b4afd7024c4
cinta <cintatafur@gmail.com>
2023-02-02 09:49:33 +0100

    ✅ Add(routes):enruto, cdn style, boostrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './pages/main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainComponent
+  },//si es vacia la ruta va a inicio
+  {
+    path:'**',
+    redirectTo: '/',
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
